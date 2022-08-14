3ccf7462955bb93e0e79bb2dc74ba0be50ccb4aa
Heo Sangmin <heosangmin@gmail.com>
2022-08-14 22:08:54 +0900

    add ch12

---
+++ rest/src/main/java/dev/mamekun/springboot/rest/service/RestTemplateService.java
@@ -0,0 +1,132 @@
+package dev.mamekun.springboot.rest.service;
+
+import dev.mamekun.springboot.rest.dto.MemberDto;
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.lang.reflect.Member;
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+
+
+    public RestTemplate restTemplate() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
+
+        HttpClient client = HttpClientBuilder.create()
+                .setMaxConnTotal(500)
+                .setMaxConnPerRoute(500)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setMaxConnTotal(500)
+                .setMaxConnPerRoute(500)
+                .build();
+
+        factory.setHttpClient(httpClient);
+        factory.setConnectTimeout(2000);
+        factory.setReadTimeout(5000);
+
+        RestTemplate restTemplate = new RestTemplate(factory);
+
+        return restTemplate;
+    }
+    public String getName() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api")
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        return responseEntity.getBody();
+    }
+
+    public String getNameWithPathVariable() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/grud-api/{name}")
+                .encode()
+                .build()
+                .expand("mamekun")
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        return responseEntity.getBody();
+    }
+
+    public String getNameWithParameter() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api/param")
+                .queryParam("name", "Mamekun")
+                .encode()
+                .build()
+                .toUri();
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+
+        return responseEntity.getBody();
+    }
+
+    public ResponseEntity<MemberDto> postWithParamAndBody() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api")
+                .queryParam("name", "Mamekun")
+                .queryParam("email", "mamekun@mamekun.dev")
+                .queryParam("organization", "Mamehome")
+                .encode()
+                .build()
+                .toUri();
+
+        MemberDto memberDto = new MemberDto();
+        memberDto.setName("mamekun!!");
+        memberDto.setEmail("mamekun@mamekun.com");
+        memberDto.setOrganization("mameStudio");
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<MemberDto> responseEntity = restTemplate.postForEntity(uri, memberDto, MemberDto.class);
+
+        return responseEntity;
+    }
+
+    public ResponseEntity<MemberDto> postWithHeader() {
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/v1/crud-api")
+                .encode()
+                .build()
+                .toUri();
+
+        MemberDto memberDto = new MemberDto();
+        memberDto.setName("MameMame");
+        memberDto.setEmail("ameame@ameame.com");
+        memberDto.setOrganization("AmeStudio");
+
+        RequestEntity<MemberDto> requestEntity = RequestEntity
+                .post(uri)
+                .header("my-header", "Mamekun API")
+                .body(memberDto);
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<MemberDto> responseEntity = restTemplate.exchange(requestEntity, MemberDto.class);
+
+        return responseEntity;
+    }
+}
